aa5c9c10e06c1d241ed8909f40122834305bdd83
Antonio De Almeida <anton@dealmeida.co>
2020-08-11 13:20:59 +0200

    Added configuration for Auth0

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,9 @@
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'r4yzvpvvk4'
+const region = "us-west-2"
+export const apiEndpoint = `https://${apiId}.execute-api.${region}.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
+  domain: 'udagram-da-dev.eu.auth0.com',
+  clientId: 'j79RMSJbUX67eVViz7ZwoJXACjttic6C',
   callbackUrl: 'http://localhost:3000/callback'
 }
